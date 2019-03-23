99cfaaa9d20354c580c9ff7eb09290689bfc1dbd
matheus <matheusbritodasilva@gmail.com>
2019-03-22 22:04:56 -0300

    rotas e controllers

---
+++ app/Http/Controllers/Controller.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Foundation\Bus\DispatchesJobs;
+use Illuminate\Routing\Controller as BaseController;
+use Illuminate\Foundation\Validation\ValidatesRequests;
+use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
+
+class Controller extends BaseController
+{
+    use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+
+    public function homepage(){
+      $variavel =  'Homepage do sistema de gestão para grupos de investimeto';
+      return view('welcome', [
+        'title' => $variavel
+      ]);
+
+    }
+    public function cadastrar(){
+      echo "tela de cadastro";
+    }
+
+    public function login(){
+      echo "tela de login";
+    }
+}
